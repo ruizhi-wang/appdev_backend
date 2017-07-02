4f5bc50907363a22964ee871b3baaf35e7257f70
ruizhi-wang <ruizhiwang90@gmail.com>
2017-07-02 12:00:16 +0100

    method added to add and delete listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,8 +8,31 @@ class ListingsController < ApplicationController
     @listing = Listing.new
   end
 
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      flash[:success] = "Listing saved"
+      redirect_to listings_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+		@listing = Listing.find(params[:id])
+		@listing.destroy
+		redirect_to listings_path
+		flash[:success] = "Item deleted"
+	end
+
   def show
     @listing = Listing.find(params[:id])
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :location)
   end
 
 end
